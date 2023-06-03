dc78d25faabdf7cecbd77c5c91530d0da5e63dc1
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-03 23:55:06 +0700

    Refactor FilterButton to fix bug with All button highlighting

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,11 +5,15 @@ const FilterButton = ({ category, selectedCategory, setSelectedCategory }) => {
     setSelectedCategory(category === "All" ? null : category);
   };
 
-  const isActive = selectedCategory === null || selectedCategory === category;
+  const isActive =
+    selectedCategory === category ||
+    (category === "All" && selectedCategory === null);
 
   return (
     <button
-      className="font-bold text-xl text-gray-800  hover:bg-gray-800 border-2 border-solid border-gray-800 hover:text-white py-4 px-8 rounded-lg"
+      className={`font-bold text-xl text-gray-800  hover:bg-gray-800 border-2 border-solid border-gray-500 hover:text-white py-4 px-8 rounded-lg ${
+        isActive ? "text-white bg-gray-800" : ""
+      }`}
       onClick={handleClick}
     >
       {category}
