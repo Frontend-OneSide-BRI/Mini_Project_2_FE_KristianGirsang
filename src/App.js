c8712f29ae564737ff6efdbb43d47c5342564627
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-04 23:02:49 +0700

    Create footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import GalleryPage from "./pages/GalleryPage";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex bottom-0 left-0 right-0 bg-gray-800 text-white text-center items-center justify-center">
+      <p className="py-5">
+        &copy; 2023{" "}
+        <span className="font-extrabold text-blue-600">imagi - </span>
+        <span className="font-extrabold text-red-600">W</span>. All rights
+        reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
